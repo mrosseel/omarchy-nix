3b9380540a177908fb369b0525d8ea393d60025b
Mike Rosseel <mike.rosseel@gmail.com>
2025-12-30 13:46:48 +0100

    Remove old elephant patch - using patched repo directly

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -1,4 +1,4 @@
-{
+inputs: {
   config,
   pkgs,
   lib,
@@ -6,6 +6,45 @@
 }: let
   cfg = config.omarchy;
   packages = import ../packages.nix {inherit pkgs config lib;};
+
+  # Override elephant packages to disable PIE hardening for Go plugin compatibility
+  # See: https://github.com/NixOS/nixpkgs/issues/211951
+  elephantWithoutPie = inputs.elephant.packages.${pkgs.system}.elephant.overrideAttrs (old: {
+    hardeningDisable = (old.hardeningDisable or []) ++ [ "pie" ];
+  });
+
+  providersWithoutPie = inputs.elephant.packages.${pkgs.system}.elephant-providers.overrideAttrs (old: {
+    hardeningDisable = (old.hardeningDisable or []) ++ [ "pie" ];
+  });
+
+  elephantCombined = pkgs.stdenv.mkDerivation {
+    pname = "elephant-with-providers";
+    version = "2.17.2-patched";
+    dontUnpack = true;
+
+    buildInputs = [
+      elephantWithoutPie
+      providersWithoutPie
+    ];
+
+    nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+    installPhase = ''
+      mkdir -p $out/bin $out/lib/elephant
+      cp ${elephantWithoutPie}/bin/elephant $out/bin/
+      cp -r ${providersWithoutPie}/lib/elephant/providers $out/lib/elephant/
+    '';
+
+    postFixup = ''
+      wrapProgram $out/bin/elephant \
+        --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
+          wl-clipboard
+          libqalculate
+          imagemagick
+          bluez
+        ])}
+    '';
+  };
 in {
   security.rtkit.enable = true;
   services.pulseaudio.enable = false;
@@ -43,18 +82,23 @@ in {
   programs.uwsm.enable = lib.mkIf cfg.seamless_boot.enable true;
 
   # Login configuration
-  services.greetd = {
+  services.greetd = let
+    # Use seamless_boot.username if set, otherwise fall back to main username
+    loginUser = if cfg.seamless_boot.username != null
+                then cfg.seamless_boot.username
+                else cfg.username;
+  in {
     enable = true;
     settings = lib.mkMerge [
       # Seamless auto-login when enabled
       (lib.mkIf cfg.seamless_boot.enable {
         initial_session = {
           command = "${pkgs.uwsm}/bin/uwsm start hyprland-uwsm.desktop";
-          user = cfg.seamless_boot.username;
+          user = loginUser;
         };
         default_session = {
           command = "${pkgs.uwsm}/bin/uwsm start hyprland-uwsm.desktop";
-          user = cfg.seamless_boot.username;
+          user = loginUser;
         };
       })
       # Traditional tuigreet when disabled
@@ -64,9 +108,35 @@ in {
     ];
   };
 
+  # Binary cache for Walker (speeds up builds)
+  nix.settings = {
+    extra-substituters = ["https://walker.cachix.org" "https://walker-git.cachix.org"];
+    extra-trusted-public-keys = [
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+    ];
+  };
+
   # Install packages
-  environment.systemPackages = packages.systemPackages;
+  environment.systemPackages = packages.systemPackages ++ [
+    inputs.walker.packages.${pkgs.system}.default
+    elephantCombined
+  ];
   programs.direnv.enable = true;
+
+  # Elephant systemd service
+  systemd.user.services.elephant = {
+    description = "Elephant launcher backend";
+    wantedBy = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+    partOf = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${elephantCombined}/bin/elephant";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+  };
 
   # Networking
   services.resolved.enable = true;
